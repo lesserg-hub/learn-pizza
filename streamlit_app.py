38412e380917099f8cfdfe9a66b81e9ae96d91ae
lesserg-hub <79595279+lesserg-hub@users.noreply.github.com>
2023-11-26 11:04:49 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,13 +7,15 @@ from statsmodels import api as sm
 
 st.sidebar.write("Sidebar")
 
-csv_file="pizza_dataset_relative_price.csv"
-df = pd.read_csv(csv_file)
-Y = df['Relative Price']
-X = df.drop(['Relative Price', 'Pizza Name', 'Topping 3_Meat', 'Topping 3_None', 'Topping 4_Fish', 'Topping 4_None',
-             'Overall Weight'], axis=1) 
-X = sm.add_constant(X)
-X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.30, random_state=1)
-model = sm.OLS(y_train, X_train).fit()
-st.write("model created")
+def create_model(csv_file):
+    df = pd.read_csv(csv_file)
+    Y = df['Relative Price']
+    X = df.drop(['Relative Price', 'Pizza Name', 'Topping 3_Meat', 'Topping 3_None', 'Topping 4_Fish', 'Topping 4_None',
+                 'Overall Weight'], axis=1) 
+    X = sm.add_constant(X)
+    X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.30, random_state=1)
+    model = sm.OLS(y_train, X_train).fit()
+    return model
+
+model = create_model(csv_file="pizza_dataset_relative_price.csv")
 st.write(model.summary())
