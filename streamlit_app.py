53e45a0df8df16ebc3404615d0e6d27be291af8c
lesserg-hub <79595279+lesserg-hub@users.noreply.github.com>
2023-11-26 10:46:25 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,18 @@
 import streamlit as st
 st.title("Projekt")
 st.write("Meine Pizza")
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from statsmodels import api as sm
+
 st.sidebar.write("Sidebar")
+
+csv_file="pizza_dataset_relative_price.csv"
+df = pd.read_csv(csv_file)
+Y = df['Relative Price']
+X = df.drop(['Relative Price', 'Pizza Name', 'Topping 3_Meat', 'Topping 3_None', 'Topping 4_Fish', 'Topping 4_None',
+             'Overall Weight'], axis=1) 
+X = sm.add_constant(X)
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.30, random_state=1)
+model = sm.OLS(y_train, X_train).fit()
+st.write("model created")
